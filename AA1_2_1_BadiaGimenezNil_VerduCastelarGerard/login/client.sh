2badc4fdb2b9f88e7fd805b2b30dd9f17167c8f3
Gerard Verdú Castelar <gerard.verdu@estudiant.enti.cat>
2026-03-06 06:34:21 +0100

    Login advances

--- AA1_2_1_BadiaGimenezNil_VerduCastelarGerard/login/client.sh
+++ AA1_2_1_BadiaGimenezNil_VerduCastelarGerard/login/client.sh
@@ -4,26 +4,31 @@ PORT=60000
 #Enviament de capçalera
 echo "HELLO" | nc -q 0 $SERVER_IP $PORT
 
+
+echo "Esperant capcalera"
 msg=$(nc -l -p $PORT)
 
-if [[ "$msg" != "OK_HEADER" ]]; then
-  echo "KO" | nc -q 0 $CLIENT_IP $PORT 
-  echo "[ERROR] Capcalera rebuda incorrecte." | tee -a $LOG_FILE
+# Divisio del missatge rebut en capçalera i salt
+serverSalt=$(echo $msg | cut -d " " -f 2)
+msg=$(echo $msg | cut -d " " -f 1)
+
+echo $serverSalt
+echo $msg
+
+# Validacio de la capçalera
+if [[ "$msg" != "HELLO" ]]; then
+  echo "KO_HEADER" | nc -q 0 $CLIENT_IP $PORT 
+  echo "[ERROR] Capcalera rebuda incorrecte."
   exit 1
 fi
-
-serverUser=$(nc -l -p $PORT)
-echo "$serverUser"
 
 read -p "Introdueix un nom d'usuari:" userName
 read -p "Introdueix una contrasenya:" password
 
 hash_password=$(printf "%s" "$password" | sha256sum | cut -d' ' -f1)
-hash_salt=$(printf "%s%s" "$hash_password" "$salt" | sha256sum | cut -d' ' -f1)
+hash_salt=$(printf "%s%s" "$hash_password" "$serverSalt" | sha256sum | cut -d' ' -f1)
 
-echo "AUTH" | nc -q 0 $SERVER_IP $PORT
-echo "$userName" | nc -q 0 $SERVER_IP $PORT
-echo "$hash_salt" | nc -q 0 $SERVER_IP $PORT
+echo "AUTH $userName $hash_salt" | nc -q 0 $SERVER_IP $PORT
 
 #Esperant rebuda de capçalera
 msg=$(nc -l -p $PORT)
--- AA1_2_1_BadiaGimenezNil_VerduCastelarGerard/login/server.sh
+++ AA1_2_1_BadiaGimenezNil_VerduCastelarGerard/login/server.sh
@@ -1,31 +1,26 @@
 CLIENT_IP="10.0.2.15"
 PORT=60000
 
-#Esperant rebuda de capçalera
+# Esperant rebuda de capçalera
 msg=$(nc -l -p $PORT)
 
+# Validacio de la capçalera rebuda
 if [[ "$msg" != "HELLO" ]]; then
-  echo "KO" | nc -q 0 $CLIENT_IP $PORT 
-  echo "[ERROR] Capcalera rebuda incorrecte." | tee -a $LOG_FILE
+  echo "KO_HEADER" | nc -q 0 $CLIENT_IP $PORT 
+  echo "[ERROR] Capcalera rebuda incorrecte."
   exit 1
+elif [[ "$msg" == "HELLO" ]]; then
+  salt=$RANDOM
+  echo "OK_HEADER $salt" | nc -q 0 $CLIENT_IP $PORT
+  echo "Connexio establerta, salt enviat."
 fi
-
-echo "Connexio establerta."
-
-salt=$RANDOM
-
-echo "OK_HEADER" | nc -q 0 $CLIENT_IP $PORT
-echo "$salt" | nc -q 0 $CLIENT_IP $PORT
-
-hash_salt=$(printf "%s" "$salt" | sha256sum | cut -d' ' -f1)
-hash_salt_salt=$(printf "%s%s" "$hash_salt" "$salt" | sha256sum | cut -d' ' -f1)
 
 
 msg=$(nc -l -p $PORT)
 
 if [[ "$msg" != "AUTH" ]]; then
   echo "KO_CAPCALERA" | nc -q 0 $CLIENT_IP $PORT 
-  echo "[ERROR] Capcalera rebuda incorrecte." | tee -a $LOG_FILE
+  echo "[ERROR] Capcalera rebuda incorrecte."
   exit 1
 fi
 
@@ -34,7 +29,7 @@ clientUsername=$(nc -l -p $PORT)
 
 if [[ "$clientUsername" != "$salt" ]]; then
   echo "KO_AUTH" | nc -q 0 $CLIENT_IP $PORT 
-  echo "[ERROR] Nom d'usuari incorrecte." | tee -a $LOG_FILE
+  echo "[ERROR] Nom d'usuari incorrecte."
   exit 1
 fi
 
@@ -42,7 +37,7 @@ fi
 clientHash=$(nc -l -p $PORT)
 if [[ "$clientHash" != "$hash_salt_salt" ]]; then
   echo "KO_AUTH" | nc -q 0 $CLIENT_IP $PORT 
-  echo "[ERROR] Nom d'usuari incorrecte." | tee -a $LOG_FILE
+  echo "[ERROR] Nom d'usuari incorrecte."
   exit 1
 fi
 
